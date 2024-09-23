31abb0ea5062f315cb742bb2af7efab0603a6f05
croussel-teluq <catherine.roussel@teluq.ca>
2024-09-23 15:02:40 -0400

    MDL-83244 fix: Problem when first letter is an accent - First letter detection switch to no accen - take 3

--- mod/glossary/view.php
+++ mod/glossary/view.php
@@ -415,7 +415,9 @@ if ($allentries) {
     }
 
     foreach ($allentries as $entry) {
-
+        $unwantedarray = array( 'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+                            'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y');
         // Setting the pivot for the current entry
         if ($printpivot) {
             $pivot = $entry->{$pivotkey};
@@ -424,8 +426,8 @@ if ($allentries) {
 
             // Reduce pivot to 1cc if necessary.
             if (!$fullpivot) {
-                $upperpivot = core_text::substr($upperpivot, 0, 1);
-                $pivottoshow = core_text::substr($pivottoshow, 0, 1);
+                $upperpivot = strtr( core_text::substr($upperpivot, 0, 1), $unwantedarray );
+                $pivottoshow = strtr( core_text::substr($pivottoshow, 0, 1), $unwantedarray );
             }
 
             // If there's a group break.
